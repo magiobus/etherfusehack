11e84985c3bf166b7ac71a9fb5aa3802c0d0d021
magiobus <aalbertobus@gmail.com>
2022-06-11 11:59:58 -0600

    Faqs

---
+++ pages/faqs.js
@@ -0,0 +1,30 @@
+import MainLayout from "@/components/layouts/MainLayout";
+import faqs from "@/data/faqs.json";
+
+const FaqsPage = () => {
+  return (
+    <MainLayout fixed={true}>
+      <div className="bg-white">
+        <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+          <h2 className="text-3xl font-extrabold text-gray-900 text-center">
+            Preguntas frecuentes
+          </h2>
+          <div className="mt-12">
+            <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-12 lg:grid-cols-3">
+              {faqs.map((faq) => (
+                <div key={faq.id}>
+                  <dt className="text-lg leading-6 font-medium text-gray-900">
+                    {faq.question}
+                  </dt>
+                  <dd className="mt-2 text-base text-gray-500">{faq.answer}</dd>
+                </div>
+              ))}
+            </dl>
+          </div>
+        </div>
+      </div>
+    </MainLayout>
+  );
+};
+
+export default FaqsPage;
