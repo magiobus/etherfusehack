3531ac6f35398217196d6cff8c7da1966002120c
magiobus <aalbertobus@gmail.com>
2022-06-11 10:44:50 -0600

    Basic Landing

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,25 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        happy: {
+          yellow: {
+            DEFAULT: "#FF9000",
+            50: "#FFE0B8",
+            100: "#FFD7A3",
+            200: "#FFC57A",
+            300: "#FFB452",
+            400: "#FFA229",
+            500: "#FF9000",
+            600: "#C77000",
+            700: "#8F5100",
+            800: "#573100",
+            900: "#1F1100",
+          },
+        },
+      },
+    },
   },
   plugins: [require("@tailwindcss/forms")],
 };
