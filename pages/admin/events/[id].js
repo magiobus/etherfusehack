4523904820040d9fea736c97b4deecb3e9606411
magiobus <aalbertobus@gmail.com>
2022-07-07 22:46:58 -0600

    Happy Yellow on switches is way better

--- pages/admin/events/[id].js
+++ pages/admin/events/[id].js
@@ -263,9 +263,9 @@ const AdminEventsShowPage = () => {
                                                   }}
                                                   className={classNames(
                                                     attendee.attended
-                                                      ? "bg-indigo-600"
+                                                      ? "bg-happy-yellow"
                                                       : "bg-gray-200",
-                                                    "relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                                    "relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-happy-yellow"
                                                   )}
                                                 >
                                                   <span className="sr-only">
