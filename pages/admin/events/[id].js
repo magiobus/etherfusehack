3d7224af41c620ea4fafe6a0d48821fa963f2a4f
magiobus <aalbertobus@gmail.com>
2022-07-20 14:45:55 -0600

    Refactor of switch component

--- pages/admin/events/[id].js
+++ pages/admin/events/[id].js
@@ -9,7 +9,7 @@ import Image from "next/image";
 import { CalendarIcon, LocationMarkerIcon } from "@heroicons/react/solid";
 import unixToFormat from "@/utils/unixToFormat";
 import classNames from "@/utils/classNames";
-import { Switch } from "@headlessui/react";
+import SwitchAttendee from "@/components/forms/fields/SwitchAttendee";
 
 const AdminEventsShowPage = () => {
   const [isInitialLoading, setIsInitialLoading] = useState(true);
@@ -249,11 +249,9 @@ const AdminEventsShowPage = () => {
                                                 Fue al evento
                                               </label>
                                               <div className="flex space-x-2">
-                                                <Switch
-                                                  checked={
+                                                <SwitchAttendee
+                                                  currentValue={
                                                     attendee.attended
-                                                      ? true
-                                                      : false
                                                   }
                                                   onChange={() => {
                                                     handleAttendance(
@@ -261,66 +259,7 @@ const AdminEventsShowPage = () => {
                                                       attendee.attended
                                                     );
                                                   }}
-                                                  className={classNames(
-                                                    attendee.attended
-                                                      ? "bg-happy-yellow"
-                                                      : "bg-gray-200",
-                                                    "relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-happy-yellow"
-                                                  )}
-                                                >
-                                                  <span className="sr-only">
-                                                    Use setting
-                                                  </span>
-                                                  <span
-                                                    className={classNames(
-                                                      attendee.attended
-                                                        ? "translate-x-5"
-                                                        : "translate-x-0",
-                                                      "pointer-events-none relative inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200"
-                                                    )}
-                                                  >
-                                                    <span
-                                                      className={classNames(
-                                                        attendee.attended
-                                                          ? "opacity-0 ease-out duration-100"
-                                                          : "opacity-100 ease-in duration-200",
-                                                        "absolute inset-0 h-full w-full flex items-center justify-center transition-opacity"
-                                                      )}
-                                                      aria-hidden="true"
-                                                    >
-                                                      <svg
-                                                        className="h-3 w-3 text-gray-400"
-                                                        fill="none"
-                                                        viewBox="0 0 12 12"
-                                                      >
-                                                        <path
-                                                          d="M4 8l2-2m0 0l2-2M6 6L4 4m2 2l2 2"
-                                                          stroke="currentColor"
-                                                          strokeWidth={2}
-                                                          strokeLinecap="round"
-                                                          strokeLinejoin="round"
-                                                        />
-                                                      </svg>
-                                                    </span>
-                                                    <span
-                                                      className={classNames(
-                                                        attendee.attended
-                                                          ? "opacity-100 ease-in duration-200"
-                                                          : "opacity-0 ease-out duration-100",
-                                                        "absolute inset-0 h-full w-full flex items-center justify-center transition-opacity"
-                                                      )}
-                                                      aria-hidden="true"
-                                                    >
-                                                      <svg
-                                                        className="h-3 w-3 text-indigo-600"
-                                                        fill="currentColor"
-                                                        viewBox="0 0 12 12"
-                                                      >
-                                                        <path d="M3.707 5.293a1 1 0 00-1.414 1.414l1.414-1.414zM5 8l-.707.707a1 1 0 001.414 0L5 8zm4.707-3.293a1 1 0 00-1.414-1.414l1.414 1.414zm-7.414 2l2 2 1.414-1.414-2-2-1.414 1.414zm3.414 2l4-4-1.414-1.414-4 4 1.414 1.414z" />
-                                                      </svg>
-                                                    </span>
-                                                  </span>
-                                                </Switch>
+                                                />
                                                 {attendee.attended ? (
                                                   <div className="badge badge-success gap-2">
                                                     Si
--- tailwind.config.js
+++ tailwind.config.js
@@ -25,21 +25,7 @@ module.exports = {
     },
   },
   daisyui: {
-    themes: [
-      {
-        mytheme: {
-          primary: "#FF9000",
-          secondary: "#D926A9",
-          accent: "#1FB2A6",
-          neutral: "#191D24",
-          "base-100": "#2A303C",
-          info: "#3ABFF8",
-          success: "#36D399",
-          warning: "#FBBD23",
-          error: "#F87272",
-        },
-      },
-    ],
+    themes: false,
   },
   plugins: [require("@tailwindcss/forms"), require("daisyui")],
 };
