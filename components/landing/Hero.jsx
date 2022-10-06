54e5406bf649e12ed02be764669da8565c0c26d1
magiobus <magio@magiobus.com>
2022-10-06 15:59:33 -0600

    Listing Cities

--- components/landing/Hero.jsx
+++ components/landing/Hero.jsx
@@ -32,12 +32,23 @@ export function Hero() {
             El hackathon de blockchain para el estado grande.
           </h1>
           <div className="mt-6 space-y-6 font-display text-2xl tracking-tight text-black">
-            <p>3 días de hacking, 3 días de aprendizaje, 3 días de creación.</p>
+            <p className="font-semibold">
+              3 días de hacking, 3 días de aprendizaje, 3 días de creación.
+            </p>
             <p>
               Un fin de semana para colaborar, aprender y crear soluciones
               digitales con blockchain. Además podrás conocer a mentores, otros
               hackers y ganar premios.
             </p>
+            <div className="sedescontainer text-lg">
+              <p className="m-0 p-0 font-semibold">
+                Inscríbete en alguna de nuestras sedes:
+              </p>
+              <p className=" m-0 p-0">
+                Cd. Chihuahua, Cd. Juárez, Nuevo Casas Grandes, Cuauhtémoc,
+                Delicias, Parral, Jiménez, Camargo.
+              </p>
+            </div>
           </div>
           <div className="mt-10 bg-black w-full flex justify-center items-center rounded-md">
             <Link href="/events">
