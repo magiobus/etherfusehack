1c581381c6b7bb0505fb85f8de9593dc20307a10
magiobus <magio@magiobus.com>
2023-01-18 20:45:14 -0600

    Exposing keys for sendinblue just for debugging for vercel

--- pages/api/sendemail.js
+++ pages/api/sendemail.js
@@ -4,6 +4,12 @@ export default async function handler(req, res) {
   const options = req.body;
 
   if (req.method === "POST") {
+    console.log("host", process.env.SMTP_HOST);
+    console.log("user", process.env.SMTP_USER);
+    console.log("pass", process.env.SMTP_PASS);
+    console.log("SENDINBLUE_API_KEY", process.env.SENDINBLUE_API_KEY);
+    console.log("SENDINBLUE_LIST_IDS", process.env.SENDINBLUE_LIST_IDS);
+
     // create reusable transporter object using the default SMTP transport
     const transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST,
