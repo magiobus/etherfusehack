cfa07eea674796b395d815554dcc762075065dbb
magiobus <magio@magiobus.com>
2023-02-10 10:43:20 -0600

    Simple Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,20 @@
 import "../styles/globals.css";
+import Script from "next/script";
 import { SessionProvider } from "next-auth/react";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
+      <Script src="https://scripts.simpleanalyticscdn.com/latest.js" />
+      <noscript>
+        {/* eslint-disable @next/next/no-img-element */}
+        <img
+          src="https://queue.simpleanalyticscdn.com/noscript.gif"
+          alt=""
+          referrerPolicy="no-referrer-when-downgrade"
+        />
+      </noscript>
     </SessionProvider>
   );
 }
