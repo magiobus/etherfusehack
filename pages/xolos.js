738397eed171eba4b5431ed95c61f3b7177ed1a7
magiobus <magio@magiobus.com>
2023-09-26 17:21:48 -0700

    static props redirect for xolos page

--- pages/xolos.js
+++ pages/xolos.js
@@ -1,14 +1,16 @@
 import { useEffect } from "react";
 
 const XolosPage = () => {
-  useEffect(() => {
-    if (window) {
-      window.location.href = "/events/6513725b78e08b60a078f1fc";
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
   return null;
 };
+
+export async function getStaticProps({ params }) {
+  return {
+    redirect: {
+      destination: "/events/6513725b78e08b60a078f1fc",
+      permanent: false,
+    },
+  };
+}
 
 export default XolosPage;
