b1667d8028dab2995c9b361e43bc58c8af1a64be
magiobus <magio@magiobus.com>
2023-09-26 17:14:00 -0700

    xolos event redirect

---
+++ pages/xolos.js
@@ -0,0 +1,14 @@
+import { useEffect } from "react";
+
+const XolosPage = () => {
+  useEffect(() => {
+    if (window) {
+      window.location.href = "/events/6513725b78e08b60a078f1fc";
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return null;
+};
+
+export default XolosPage;
